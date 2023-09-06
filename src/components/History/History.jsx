45c2a8cff29e06e5002443dcaf618bc30b203a1e
Anton <sokolanton.1998@gmail.com>
2023-09-06 18:08:23 +0300

    clear history btn added

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,22 +1,33 @@
 import css from "./History.module.css";
+import { useState } from "react";
 
 export function History({ toggleHistory }) {
+  const [history, setHistory] = useState(() => {
+    const storedHistory = localStorage.getItem("calculatorHistory");
+    return JSON.parse(storedHistory) || [];
+  });
+
   const historyClassName = toggleHistory ? css.historyOpen : css.historyClose;
-  const storedHistory = localStorage.getItem("calculatorHistory");
-  const parsedHistory = JSON.parse(storedHistory);
-  //   console.log(storedHistory);
+
+  function clearHistory() {
+    localStorage.clear();
+    setHistory([]);
+  }
 
   return (
     <>
       <div className={historyClassName}>
         {/* <h2>history</h2> */}
         <ul className={css.list}>
-          {parsedHistory.map((item, index) => (
+          {history.map((item, index) => (
             <li key={index} className={css.item}>
               {item}
             </li>
           ))}
         </ul>
+        <button className={css.clearBtn} onClick={clearHistory}>
+          🗑
+        </button>
       </div>
     </>
   );
