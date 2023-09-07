07a2144c1dbc91b159fbf2d8c9c9f86249e20c6a
Anton <sokolanton.1998@gmail.com>
2023-09-07 22:40:43 +0300

    custom scroll for history

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,4 +1,5 @@
 import css from "./History.module.css";
+import { Scrollbars } from "react-custom-scrollbars";
 
 export function History({ toggleHistory, history, clearHistory }) {
   const historyClassName = toggleHistory ? css.historyOpen : css.historyClose;
@@ -9,17 +10,23 @@ export function History({ toggleHistory, history, clearHistory }) {
   return (
     <div className={historyClassName}>
       <div className={historySecClassName}>
-        {history.length === 0 ? (
-          <p className={css.noHistory}>No history yet</p>
-        ) : (
-          <ul className={css.list}>
-            {history.map((item, index) => (
-              <li key={index} className={css.item}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        )}
+        <Scrollbars
+          renderThumbVertical={(props) => (
+            <div {...props} style={{ backgroundColor: "#ea6800" }} />
+          )}
+        >
+          {history.length === 0 ? (
+            <p className={css.noHistory}>No history yet</p>
+          ) : (
+            <ul className={css.list}>
+              {history.map((item, index) => (
+                <li key={index} className={css.item}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
+        </Scrollbars>
         <button className={css.clearBtn} onClick={clearHistory}>
           🗑
         </button>
